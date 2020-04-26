70f80504f624f692dbd2cb98d1d02419b2abf4d5
RebeccaOlatunde <54245689+RebeccaOlatunde@users.noreply.github.com>
2020-04-26 17:14:24 -0400

    Update Program.cs

--- Lab_6/Program.cs
+++ Lab_6/Program.cs
@@ -1,12 +1,78 @@
-﻿using System;
+Using System;
 
 namespace Lab_6
 {
-    class Program
+    class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int[] Dice1 = new int[6];
+            int[] Dice2 = new int[6];
+
+            //Declaring variables
+
+            string response;
+            string sides;
+            int sides1;
+            bool isItANum;
+            int diceRoll = 0;
+
+            do
+            {
+
+                Console.WriteLine("Welcome to GrandCircus Casino. Roll the dice (y/n?)");
+                response = Console.ReadLine().ToLower();
+
+
+                Console.WriteLine("How many sides does each dice have?"); //Enter 6
+                sides = Console.ReadLine();
+                isItANum = int.TryParse(sides, out sides1);
+
+                // Dice Roll Prompt
+                
+                    Console.WriteLine("Roll");
+
+                    diceRoll = rnd.Next(6);
+                    Dice1[diceRoll]++;
+                    Console.WriteLine("Dice1: {0}", diceRoll + 1);
+
+                    diceRoll = rnd.Next(6);
+                    Dice1[diceRoll]++;
+                    Console.WriteLine("Dice2: {0}", diceRoll + 1);
+
+                // Subsequent Dice Roll
+
+                    Console.WriteLine("Roll Dice again? (y/n?)");
+
+                    response = Console.ReadLine().ToLower();
+
+            } while (response == "y") ;
+                Console.WriteLine("GoodBye");
+                Console.ReadLine();
+
+
+           // Welcome to GrandCircus Casino. Roll the dice(y/n?)
+           // Response: y
+           // How many sides does each dice have?
+           // Response: 6
+           // Roll
+           // Dice1: 5 (number randomly chosen ny app)
+           // Dice2: 4 (number randomly chosen by app)
+           // Roll Dice again? (y/n?)
+           // If "y" it continues, if "n" then Goodbye.
+
+
+         }   
+    }
+}    
+
+
+
+
+
+
+
         }
     }
 }
